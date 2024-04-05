d6afbd82ac170a72ed5a03d94ae75070bb1b34dd
Mykhailo Gogol <mishhagogol@gmail.com>
2024-04-05 11:54:10 +0300

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import './App.css'
 
 function App() {
   return (
-    <div>
-      <div className="flex justify-center items-center">
-        <h1 className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-5xl leading-normal font-black">
-          Sugar Honey Iced Tea!
-        </h1>
-      </div>
+    <div className=" bg-gray-800 min-h-screen min-w-80 flex justify-center items-center">
+      <h1 className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-5xl leading-normal font-black">
+        Sugar Honey Iced Tea!
+      </h1>
     </div>
   )
 }
